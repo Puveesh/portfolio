791e2823e571fd8e339cb490e952519e13882461
Puveesh Keshav M <727724eucs197@skcet.ac.in>
2026-06-12 14:11:50 +0530

    Added portfolio project

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import LoadingScreen from "./components/LoadingScreen";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 
 export default function App() {
